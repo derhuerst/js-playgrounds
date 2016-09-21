3d4a8a4614fe1789e1ce138ce0e3af166b7a1677
Jannis R <mail@jannisr.de>
2016-09-21 23:10:38 +0200

    code analysis

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
 'use strict'
 
-// todo
+const blessed = require('blessed')
+const inspect = require('inspect-code')
+
+
+
+const lineBreak = /[\r\n]+/g
+
+const calculate = (code) => {
+	const lines = code.split(lineBreak).map((line) => line.length)
+	const markers = []
+
+	try {
+		const results = inspect(code)
+		for (let result of results) {
+			const part = code.substring(0, result.to + 1).trim()
+			if (!part) continue
+			const breaks = part.match(lineBreak)
+			const top = breaks ? breaks.length : 0
+			markers.push({
+				top, left: lines[top] + 2,
+				content: result.values[result.values.length - 1] + '',
+				style: {fg: 'blue', underline: true}
+			})
+		}
+	} catch (err) {
+		if (!err.loc) return []
+		const top = err.loc.line - 1
+		markers.push({
+			top, left: lines[top] + 2,
+			content: err.message,
+			style: {fg: 'red', underline: true}
+		})
+	}
+
+	return markers
+}
+
+
+
+let markers = []
+let before = ''
+
+const onKeypress = (editor) => () => {
+	const code = editor.textBuf.getText()
+	if (code === before) return
+	before = code
+
+	for (let marker of markers)
+		editor.parent.remove(marker)
+
+	process.nextTick(() => {
+		markers = calculate(code).map((m) => blessed.text(m))
+
+		for (let marker of markers)
+			editor.parent.append(marker)
+
+		editor.parent.render()
+	})
+}
+
+module.exports = onKeypress
